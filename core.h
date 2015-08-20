9c4d70e6f87b2c3705a36861e406639c4b5b8090
Ahmed mohamed abd el mawgood <ahmedsoliman0x666@gmail.com>
2015-08-20 15:02:53 +0200

    adding some modularization to the code and moving some large chunks of code that will be reused into macros for simplicity

--- core.h
+++ core.h
@@ -10,12 +10,29 @@ char_n number of characters
 struct font{
 unsigned int c;
 unsigned int *r;
-const int d_n;
+unsigned int d_n;
 int (*allowed)(int);
 int (*index)(int);
 int (*init)(void);
 char ***d;
 };
+#define font_alloc(font,column,nofdesigns)				\
+	font.d_n = nofdesigns; 						\
+	font.c = column;						\
+	font.r = malloc(sizeof(int)*font.d_n);				\
+        font.d = malloc(sizeof(char***)*font.d_n);			\
+	for(int i=0;i<font.d_n;i++)					\
+                font.d[i] =malloc(sizeof(char**)*font.c)
+#define ARRAY(...) __VA_ARGS__
+#define d_alloc(font,_char,row,design)					\
+	font.r[font.index(_char)] = row;				\
+	for(int j=0;j<font.c;j++)					\
+		font.d[font.index(_char)][j] = 				\
+		malloc(sizeof(char*)*font.r[font.index(_char)]);	\
+	char *temp[] = design;						\
+	for(int i=0;i<whimsy.c;i++)					\
+		strcpy(whimsy.d[0][i],temp[i])
+
 int printnarts(char* text, int n, struct font* style)
 {
 	for(int i=0;i<n;i++)
--- whimsy.h
+++ whimsy.h
@@ -13,27 +13,15 @@ return  islower(c)? c -'a'+1 :0;
 }
 int whimsy_init(void);
 struct font whimsy = {
-	.d_n = 27, // a-z + space
 	.allowed = whimsy_allowed,
 	.index = whimsy_index,
 	.init = whimsy_init
 };
 int whimsy_init(void){
 	//data allocation
-	whimsy.r = malloc(sizeof(int)*whimsy.d_n);
-	whimsy.d = malloc(sizeof(char***)*whimsy.d_n);
-	whimsy.c = 10;
-	whimsy.r[0] = 4;
-	for(int i=1;i<27;i++) whimsy.r[i] =1;
-	for(int i=0;i<whimsy.d_n;i++)
-		whimsy.d[i] =malloc(sizeof(char**)*whimsy.c);
-	for(int i=0;i<whimsy.d_n;i++)
-		for(int j=0;j<whimsy.c;j++)
-			whimsy.d[i][j] = malloc(sizeof(char*)*whimsy.r[i]);
-	char *a[] ={"    ","    ","    ","    ","    ","    ",
-		    "    ","    ","    ","    "};
-	for(int i=0;i<whimsy.c;i++)
-		strcpy(whimsy.d[0][i],a[i]);
+	font_alloc(whimsy,10,27);
+	d_alloc(whimsy,' ',5,ARRAY({"    ","    ","    ","    ","    ",	\
+				  "    ","    ","    ","    ","    ",}));
 	return 0;
 }
 #endif
