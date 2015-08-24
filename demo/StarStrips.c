c57392e182bba9dc065cc83d03bc9a08428e851c
Ahmed mohamed abd el mawgood <ahmedsoliman0x666@gmail.com>
2015-08-24 23:26:25 +0200

    using fgets instead of gets in the demos

--- demo/StarStrips.c
+++ demo/StarStrips.c
@@ -1,9 +1,11 @@
 #include <StarStrips.h>
 #include <stdio.h>
+#include <string.h>
 int main(){
 	printf("enter some text\n");
 	char s[100];
-	gets(s);	
+	fgets(s,99,stdin);
+	s[strlen(s)-1] =0;	
 	StarStrips.init();
 	StarStrips.print(s);
 	StarStrips.exit();
--- demo/whimsy.c
+++ demo/whimsy.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 #include <whimsy.h>
 int main(){
 	printf("enter some text\n");
 	char s[100];
-	gets(s);	
+	fgets(s,99,stdin);
+	s[strlen(s)-1] = 0;
 	whimsy.init();
 	whimsy.print(s);
 	whimsy.exit();
